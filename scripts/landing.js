4bd891c2e2c6819e6c8a19d6c12c0cca7aa30feb
addi110 <addi@rizwanreza.com>
2017-06-29 22:15:56 -0700

    Checkpoint 8 DOM Scripting: Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,27 +1,27 @@
-var animatePoints = function (){
+var animatePoints = function () {
 
     var points = document.getElementsByClassName('point');
 
     var revealPointOne = function () {
         points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)"
-        points[0].style.WebKitTransform = "scaleX(1) translateY(0)";
-  };
+        points[0].style.transform = 'scaleX(1) translateY(0)';
+        points[0].style.msTransform = 'scaleX(1) translateY(0)';
+        points[0].style.WebKitTransform = 'scaleX(1) translateY(0)';
+    };
 
     var revealPointTwo = function () {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebKitTransform = "scaleX(1) translateY(0)";
-  };
+        points[1].style.opacity =1;
+        points[1].style.transform = 'scaleX(1) translateY(0)';
+        points[1].style.msTransform = 'scaleX(1) translateY(0)';
+        points[1].style.WebKitTransform = 'scaleX(1) translateY(0)';
+    };
 
     var revealPointThree = function () {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebKitTransform = "scaleX(1) translateY(0)";
-  };
+        points[2].style.opacity =1;
+        points[2].style.transform = 'scaleX(1) translateY(0)';
+        points[2].style.msTransform = 'scaleX(1) translateY(0)';
+        points[2].style.WebKitTransform = 'scaleX(1) translateY(0)';
+    };
 
     revealPointOne();
     revealPointTwo();
