aa939d6f656e3fc72c4851a1663b3da551736c5e
addi110 <addi@rizwanreza.com>
2017-06-30 15:26:57 -0700

    Completed Checkpoint 9

--- scripts/landing.js
+++ scripts/landing.js
@@ -14,19 +14,18 @@ var pointsArray = document.getElementsByClassName('point');
     }
 };
 
-window.onload = function () {
+window.onload = function() {
 
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
 
-    
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollingDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-     window.addEventListener('scroll', function(event) {
-          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollingDistance) {
-              animatePoints(pointsArray);
+    window.addEventListener("scroll", function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
         }
     });
 }
