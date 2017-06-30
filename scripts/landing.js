53cf0d977222e826c9a0b12b0af8d8643d2cfd40
addi110 <addi@rizwanreza.com>
2017-06-30 09:39:31 -0700

    Assignment for checkpoint 8 Completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,10 @@
-var animatePoints = function () {
+var revealPoint = function () {
 
     var points = document.getElementsByClassName('point');
 
-    var revealPointOne = function () {
-        points[0].style.opacity = 1;
-        points[0].style.transform = 'scaleX(1) translateY(0)';
-        points[0].style.msTransform = 'scaleX(1) translateY(0)';
-        points[0].style.WebKitTransform = 'scaleX(1) translateY(0)';
-    };
-
-    var revealPointTwo = function () {
-        points[1].style.opacity =1;
-        points[1].style.transform = 'scaleX(1) translateY(0)';
-        points[1].style.msTransform = 'scaleX(1) translateY(0)';
-        points[1].style.WebKitTransform = 'scaleX(1) translateY(0)';
-    };
-
-    var revealPointThree = function () {
-        points[2].style.opacity =1;
-        points[2].style.transform = 'scaleX(1) translateY(0)';
-        points[2].style.msTransform = 'scaleX(1) translateY(0)';
-        points[2].style.WebKitTransform = 'scaleX(1) translateY(0)';
-    };
-
-    revealPointOne();
-    revealPointTwo();
-    revealPointThree();
-
+    for (var i = 0; i < points.length; i++) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+    }
 
 };
